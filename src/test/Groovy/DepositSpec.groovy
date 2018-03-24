3225dda660d24cf0118c2717d4053230acd5db0e
msaraga <masaraga@astek.fr>
2018-03-24 08:54:50 +0100

    Deposit spec

--- src/test/Groovy/DepositSpec.groovy
+++ src/test/Groovy/DepositSpec.groovy
@@ -1,7 +1,24 @@
+import kata.bank.project.model.account.CurrentAccount
+import kata.bank.project.service.TransactionService
 import spock.lang.Specification
 
 class DepositSpec extends Specification {
 
-    def 'As a bank client'
+    def 'As a bank client I want to make an initial deposit'() {
+
+        given:
+        def account = new CurrentAccount()
+        def service = new TransactionService()
+
+        def initialSize = account.operations.size()
+
+        when:
+        service.makeDeposit(account, 1000.0)
+
+        then:
+        assert account.operations.size() == initialSize + 1
+        assert account.amount == 1000.0
+
+    }
 
 }
